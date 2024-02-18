f2fe9eb330bd862c6dbe0dd371ed7ba58361491c
Hiro527 <hiro527.dev@gmail.com>
2024-02-18 15:35:27 +0900

    Add: 認証周りの追加

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,70 @@
 import { Context } from 'hono';
 import { D1QB } from 'workers-qb';
 import { getCookie } from 'hono/cookie';
+import { TOKEN_LENGTH } from './consts';
+import { v4 } from 'uuid';
+
+export const generateSaltHash = async (str: string) => {
+    const saltArray = new Uint8Array(16);
+    const salt = String.fromCharCode(...crypto.getRandomValues(saltArray));
+    const strUint8 = new TextEncoder().encode(str + salt);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', strUint8);
+    const hashHex = Array.from(new Uint8Array(hashBuffer))
+        .map((v) => {
+            return v.toString(16).padStart(2, '0');
+        })
+        .join('');
+    return { salt, hashHex };
+};
+
+export const challangePassword = async (password: string, salt: string, hash: string) => {
+    const strUint8 = new TextEncoder().encode(password + salt);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', strUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((v) => {
+            return v.toString(16).padStart(2, '0');
+        })
+        .join('');
+
+    return hashHex === hash;
+};
+
+export const createNewSession = async (c: Context<ContextArgs>, user: User) => {
+    const qb = new D1QB(c.env.DB);
+
+    const arr = new Uint8Array(32);
+    const token = btoa(String.fromCharCode(...crypto.getRandomValues(arr))).slice(0, TOKEN_LENGTH);
+    const expire_date = Math.floor(new Date().getTime() + 3600 * 24 * 30 * 1e3);
+
+    await qb
+        .insert({
+            tableName: 'tokens',
+            data: {
+                token: token,
+                user_uuid: user.uid,
+                expire_date: expire_date,
+            },
+        })
+        .execute();
+
+    const session_uuid = v4();
+
+    const session_expire_date = Math.floor(new Date().getTime() / 1e3 + 60);
+
+    await qb
+        .insert({
+            tableName: 'sessions',
+            data: {
+                id: session_uuid,
+                token: token,
+                expire_date: session_expire_date,
+            },
+        })
+        .execute();
+
+    return session_uuid;
+};
 
 export const authUserByToken = async (c: Context<ContextArgs>) => {
     const qb = new D1QB(c.env.DB);
@@ -33,58 +97,30 @@ export const authUserByToken = async (c: Context<ContextArgs>) => {
     return user;
 };
 
-export const generateSaltHash = async (str: string) => {
-    const saltArray = new Uint8Array(16);
-    const salt = String.fromCharCode(...crypto.getRandomValues(saltArray));
-    const strUint8 = new TextEncoder().encode(str + salt);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', strUint8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-        .map((v) => {
-            return v.toString(16).padStart(2, '0');
-        })
-        .join('');
-    return { salt, hashHex };
-};
-
-export const challangePassword = async (challenger: string, salt: string, hash: string) => {
-    const strUint8 = new TextEncoder().encode(challenger + salt);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', strUint8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-        .map((v) => {
-            return v.toString(16).padStart(2, '0');
-        })
-        .join('');
-
-    if (hashHex === hash) {
-        return 1;
-    }
-    return 0;
-};
-
-export const challangeToken = async (qb: D1QB, token: string) => {
-    const fethedTokenInfo = await qb
+export const challangeToken = async (qb: D1QB, tokenToChallange: string) => {
+    const fetchedToken = await qb
         .fetchOne({
             tableName: 'tokens',
             fields: '*',
             where: {
                 conditions: 'token = ?1',
-                params: [token],
+                params: [tokenToChallange],
             },
         })
         .execute();
 
-    if (!fethedTokenInfo.results) {
-        return 0;
+    const token = fetchedToken.results as Token;
+
+    if (!token) {
+        return null;
     }
 
-    const user_uuid = fethedTokenInfo.results.user_uuid;
+    const uid = token.uid;
 
-    const expired = new Date().getTime() > fethedTokenInfo.expire_date * 1e3;
+    const isExpired = new Date().getTime() > token.expire_date;
 
-    if (expired) {
-        return 0;
+    if (isExpired) {
+        return null;
     }
 
     const fetchedUser = await qb
@@ -92,12 +128,12 @@ export const challangeToken = async (qb: D1QB, token: string) => {
             tableName: 'users',
             fields: '*',
             where: {
-                conditions: 'uuid = ?1',
-                params: [user_uuid],
+                conditions: 'uid = ?1',
+                params: [uid],
             },
         })
         .execute();
 
-    const user = fetchedUser.results as User;
+    const user = fetchedUser.results as Auth;
     return user;
 };
